26777db51f11e2ce194e91890296ccb245e2f4a8
Tom Johnson <tom.johnson@zirmed.com>
2019-09-26 12:58:25 -0400

    new check in

---
+++ MazeDLL/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MazeDLL
+{
+    public abstract class Directions
+    {
+        public enum Direction
+        {
+            North, South, East, West
+        }
+    }
+}
---
+++ MazeGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using MazeDLL;
+using System;
+using System.Diagnostics;
+
+namespace MazeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 1; x++)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                Maze maze = new Maze();
+
+                maze.Generate(10, 10, 50);
+                sw.Stop();
+
+                Console.WriteLine(sw.ElapsedMilliseconds);
+            }
+        }
+    }
+}
